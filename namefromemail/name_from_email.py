503c8fc97a0547e3b28999f71ba7edb165ac20a9
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-19 10:01:43 +0200

    feat(name_from_email): working

--- namefromemail/name_from_email.py
+++ namefromemail/name_from_email.py
@@ -5,4 +5,15 @@
 # example: "elek.viz@exam.com" for this input the output should be: "Viz Elek"
 # accents does not matter
 
+def name_from_email(email_address):
+    at_sign = email_address.index('@')
+    name_part = email_address[ : at_sign]
+    dot_position = name_part.index('.')
+    first_name = name_part[ : dot_position]
+    last_name = name_part[dot_position + 1 : ]
+    capitalized_format = last_name.capitalize() + ' ' + first_name.capitalize()
+    return capitalized_format
+
+
 print(name_from_email("elek.viz@exam.com"))
+
