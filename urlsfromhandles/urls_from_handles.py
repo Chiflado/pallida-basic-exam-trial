21abdab5c2c4de3046df742b10f51e467c93c901
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-19 11:39:34 +0200

    feat(urls_from_handle): working

--- urlsfromhandles/urls_from_handles.py
+++ urlsfromhandles/urls_from_handles.py
@@ -8,4 +8,14 @@
 # output: ["https://github.com/greenfox-academy/ghhandle1", "https://github.com/greenfox-academy/ghhandle2"]
 
 names = ["ghhandle1", "ghhandle2"]
+
+def urls_from_handles(names):
+    list_of_url = []
+    for i in range(len(names)):
+        generated_url = "https://github.com/greenfox-academy/" + names[i]
+        list_of_url = list_of_url + [generated_url]
+    return list_of_url
+        
+
+
 print(urls_from_handles(names))
